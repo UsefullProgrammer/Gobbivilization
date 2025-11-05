5284d5556920d9b8bd6aa76e8cd3a26010fc6c59
GHelper <gobbi.office@gmail.com>
2025-11-05 01:20:53 +0100

    Tolto file inutili.

--- UPDATE/Assets/UI/FrontEnd/LegalScreen.lua
+++
@@ -1,5 +0,0 @@
--------------------------------------------------
--------------------------------------------------
-Controls.ContinueButton:RegisterCallback(Mouse.eLClick, function()
-	UIManager:DequeuePopup(ContextPtr);
-end);
