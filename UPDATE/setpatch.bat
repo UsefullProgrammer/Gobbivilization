bf909bc51f42d126446ceddb414bf78b504ee586
GHelper <gobbi.office@gmail.com>
2025-11-15 02:24:08 +0100

    Miglioramenti traduzioni. Funnny modifiche.

--- UPDATE/setpatch.bat
+++ UPDATE/setpatch.bat
@@ -1,4 +1,4 @@
-rcedit-x64.exe "CivilizationV.exe" --set-version-string FileVersion "1, 0, 3, 281, (403700) (15/11/2025)"
+rcedit-x64.exe "CivilizationV.exe" --set-version-string FileVersion "1, 0, 3, 280, (403700) (15/11/2025)"
 rcedit-x64.exe "CivilizationV.exe" --set-version-string ProductVersion "1, 0, 3, 279, (403694) (11/19/2014)"
-rcedit-x64.exe "CivilizationV_DX11.exe" --set-version-string FileVersion "1, 0, 3, 281, (403700) (15/11/2025)"
+rcedit-x64.exe "CivilizationV_DX11.exe" --set-version-string FileVersion "1, 0, 3, 280, (403700) (15/11/2025)"
 pause
