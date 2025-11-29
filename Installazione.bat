da84f2f0058b3dd0622e788153119a07cce9373b
GHelper <gobbi.office@gmail.com>
2025-11-29 13:40:11 +0100

    Base e cancelalzione file che danno problemi

--- Installazione.bat
+++ Installazione.bat
@@ -105,7 +105,8 @@ rcedit-x64.exe "%DEST%/CivilizationV.exe" --set-version-string FileVersion "1, 0
 rcedit-x64.exe "%DEST%/CivilizationV_DX11.exe" --set-version-string FileVersion "1, 0, 3, 279, (403700) (15/11/2025)"
 ::echo correggo eventuali versioni sbagliate da precedenti settaggi
 rcedit-x64.exe "%DEST%/CivilizationV.exe" --set-version-string ProductVersion "1, 0, 3, 279, (403694) (11/19/2014)"
-
+::file obsoleti
+del  "%DEST%\Assets\DLC\Shared\Gameplay\XML\Buildings\CIV5Buildings.xml" /f /q
 
 echo Installazione completata.
 echo.
