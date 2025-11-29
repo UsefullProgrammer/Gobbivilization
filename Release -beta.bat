26ecd339d57272d72a432d3fa3654d24686d1351
GHelper <gobbi.office@gmail.com>
2025-11-29 01:19:10 +0100

    MIgliorato la religione. MIgliorato la grecia. MIgliorato venezia.

---
+++ Release -beta.bat
@@ -0,0 +1,19 @@
+@echo off
+setlocal
+
+:: Definisci la directory di origine (root) e destinazione (release)
+set "SOURCE=%~dp0"
+set "DESTINATION=%SOURCE%beta"
+
+:: Crea la cartella di destinazione se non esiste
+if not exist "%DESTINATION%" (
+    mkdir "%DESTINATION%"
+)
+
+:: Copia la cartella UPDATE
+xcopy "%SOURCE%UPDATE" "%DESTINATION%\Beta" /E /I /Y
+xcopy "%SOURCE%ProblemSolver" "%DESTINATION%\ProblemSolver" /E /I /Y
+:: Copia i file specificati
+copy "%SOURCE%Disintallazione beta.bat" "%DESTINATION%" /Y
+echo Operazione completata con successo.
+pause
---
+++ beta/beta/Assets/UI/FrontEnd/FrontEnd.lua
@@ -0,0 +1,18 @@
+-------------------------------------------------
+-- FrontEnd
+-------------------------------------------------
+
+function ShowHideHandler( bIsHide, bIsInit )
+
+		-- Check for game invites first.  If we have a game invite, we will have flipped 
+		-- the Civ5App::eHasShownLegal and not show the legal/touch screens.
+
+    if( not bIsHide ) then
+        Controls.AtlasLogo:SetTexture( "CivilzationVAtlas.dds" );
+    	UIManager:SetUICursor( 0 );
+        UIManager:QueuePopup( Controls.MainMenu, PopupPriority.MainMenu );
+    else
+        Controls.AtlasLogo:UnloadTexture();
+    end
+end
+ContextPtr:SetShowHideHandler( ShowHideHandler );
